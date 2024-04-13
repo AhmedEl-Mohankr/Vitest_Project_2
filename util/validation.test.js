dba8520a316a4133d87781ee6c2b58b34cca28dc
AhmedEl-Mohankr <96636877+AhmedEl-Mohankr@users.noreply.github.com>
2024-04-13 14:02:32 +0100

    Update validation.test.js

--- util/validation.test.js
+++ util/validation.test.js
@@ -1,19 +1,16 @@
 import { describe, it, expect } from "vitest";
 import { validateNotEmpty } from "./validation";
 
-//Testing for errors *( note that the following pattern can be used for testing errors).
+
 it("should throw an error if an empty string is provided as a value", () => {
   const testInput = "";
-  //The validation function is wrapper for my validation function
-  //To validate not empty, we will pass the testInput
   const validationfn = () => validateNotEmpty(testInput);
   expect(validationfn).toThrow();
 });
 
 it("should throw an error if an empty string is provided as a value", () => {
   const testInput = "  ";
-  //The validation function is wrapper for my validation function
-  //To validate not empty, we will pass the testInput
+  
   const validationfn = () => validateNotEmpty(testInput);
   expect(validationfn).toThrow();
 });
